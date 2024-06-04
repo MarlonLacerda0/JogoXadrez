532690ef6a82e471a1a37aa0fcb53bd873ddda3e
Marlon Lacerda <marlonlac10@hotmail.com>
2024-06-03 21:42:32 -0300

    Controlando as pecas capturadas

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -17,10 +17,7 @@ namespace xadrez_console // Note: actual namespace depends on the project name.
                     try
                     {
                         Console.Clear();
-                        Tela.ImprimirTabuleiro(partida.Tab);
-                        Console.WriteLine();
-                        Console.WriteLine("Turno: " + partida.Turno);
-                        Console.WriteLine("Aguardando jogada: " + partida.JogadorAtual);
+                        Tela.ImprimirPartida(partida);
 
                         Console.WriteLine();
                         Console.Write("Origem: ");
